7ee77cd50712e4e6b477b9d382193c9d1d77058e
Lans12345 <olanalans12345@gmail.com>
2024-12-07 08:45:01 +0800

    initialy added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
-import 'package:geobound_web/screens/auth/login_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:geobound_web/screens/home_screen.dart';
+import 'package:geobound_web/screens/auth/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//       options: const FirebaseOptions(
+//           apiKey: "AIzaSyAlT6C46rXK3Ogip4p7D8JjUZKngMbGRwk",
+//           authDomain: "attendance-checker-63276.firebaseapp.com",
+//           databaseURL:
+//               "https://attendance-checker-63276-default-rtdb.asia-southeast1.firebasedatabase.app",
+//           projectId: "attendance-checker-63276",
+//           storageBucket: "attendance-checker-63276.firebasestorage.app",
+//           messagingSenderId: "52667528148",
+//           appId: "1:52667528148:web:38adb152afccb71d539c41",
+//           measurementId: "G-5Q8MEEFXK9"));
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
